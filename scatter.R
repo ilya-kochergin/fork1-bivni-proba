f423b04204073c549e5a0260cf52c376fd43dd63
Ilya Kochergin <ikochergin@gmail.com>
2015-09-23 21:38:45 +0300

    изменили параметры plot() и убрали title()

--- scatter.R
+++ scatter.R
@@ -1,3 +1,7 @@
-plot(mtcars$disp,mtcars$mpg)
-title(main="зависимость расхода топлива от объема двигателя")
-title(xlab="cubic inches")
+plot(
+  mpg ~ disp, 
+  data = mtcars,
+  main = "зависимость расхода топлива от объема двигателя",
+  ylab="миль на галлон",
+  xlab = "куб дюймы"
+)
